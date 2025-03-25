548b24fde068f303f47d0f2f3ba4b18f886ed51f
Edwin <edwin.lry@gmail.com>
2025-03-25 16:32:30 +0000

    finish task 1.1 for without course

--- src/main/java/model/FAQ.java
+++ src/main/java/model/FAQ.java
@@ -11,6 +11,20 @@ public class FAQ {
         section.setParent(null);
     }
 
+    public void removeSection(FAQSection section) {
+        sections.remove(section);
+        promoteSection(section);
+    }
+
+    private void promoteSection(FAQSection section) {
+        if (section.getParent() == null) {
+            return;
+        }
+        FAQSection parent = section.getParent();
+        sections.add(parent);
+        parent.setParent(null);
+    }
+
     public List<FAQSection> getSections() {
         return sections;
     }
