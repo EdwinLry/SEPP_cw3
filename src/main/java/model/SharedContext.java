d3399e6199b66ae0d22dd91eacd81990e35ba805
Zoe Sztandke <zoe.sztandke@outlook.com>
2025-04-01 11:34:14 +0100

    Removed FAQ subscription from SharedContext

--- src/main/java/model/SharedContext.java
+++ src/main/java/model/SharedContext.java
@@ -1,7 +1,5 @@
 package model;
 
-import controller.AdminStaffController;
-
 import java.util.*;
 
 public class SharedContext {
@@ -12,38 +10,18 @@ public class SharedContext {
     public final FAQManager faqManager;
 
     public final CourseManager courseManager;
-    private final Map<String, Set<String>> faqTopicsUpdateSubscribers;
 
     public SharedContext() {
         this.currentUser = new Guest();
         this.inquiries = new ArrayList<>();
         faqManager = new FAQManager();
         courseManager = new CourseManager();
-        faqTopicsUpdateSubscribers = new HashMap<>();
     }
 
     public FAQManager getFAQ() {
         return faqManager;
     }
 
-    public boolean registerForFAQUpdates(String email, String topic) {
-        if (faqTopicsUpdateSubscribers.containsKey(topic)) {
-            return faqTopicsUpdateSubscribers.get(topic).add(email);
-        } else {
-            Set<String> subscribers = new HashSet<>();
-            subscribers.add(email);
-            faqTopicsUpdateSubscribers.put(topic, subscribers);
-            return true;
-        }
-    }
-
-    public boolean unregisterForFAQUpdates(String email, String topic) {
-        return faqTopicsUpdateSubscribers.getOrDefault(topic, new HashSet<>()).remove(email);
-    }
-
-    public Set<String> usersSubscribedToFAQTopic(String topic) {
-        return faqTopicsUpdateSubscribers.getOrDefault(topic, new HashSet<>());
-    }
 
     public CourseManager getCourseManager() {
         return courseManager;
