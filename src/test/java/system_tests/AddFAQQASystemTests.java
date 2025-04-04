538576848aae4b6053dde1842297eae2dde99f0d
Leila Fiske de Gouveia <leila.fiske@icloud.com>
2025-04-04 14:18:40 +0100

    fixed tests

--- src/test/java/system_tests/AddFAQQASystemTests.java
+++ src/test/java/system_tests/AddFAQQASystemTests.java
@@ -31,7 +31,7 @@ public class AddFAQQASystemTests extends TUITest {
         controller.manageFAQ();
 
         assertOutputContains("Created topic 'Topic 1'");
-        assertOutputContains("Created new FAQ item");
+        assertOutputContains("New FAQ item was added without tag.");
     }
 
 
@@ -39,19 +39,83 @@ public class AddFAQQASystemTests extends TUITest {
      * Tests adding a new FAQ item with a course tag.
      */
     @Test
-    public void testAddFAQQAWithTag() throws URISyntaxException, IOException, ParseException {
+    public void testWithCourseTag() throws URISyntaxException, IOException, ParseException {
         SharedContext context = new SharedContext();
         loginAsAdminStaff(context);
 
-        // Add a course
-        setMockInput("3", "-2", "INF2B", "SEPP", "Software Engineering and Professional Practise", "n", "Mrs A", "mrsa@ed.ac.uk",
-                "Mr B", "mrb@ed.ac.uk", "1", "3", "-1");
+        setMockInput(
+                // Add course
+                "-2", "INF2B", "SEPP", "Software Engineering and Professional Practice",
+                "n", "Mrs A", "mrsa@ed.ac.uk",
+                "Mr B", "mrb@ed.ac.uk", "1", "3", "-1", "0",
 
-        setMockInput("2", "-2",             // Go to manage FAQ → Add FAQ
-                "Topic 2",     // New topic
-                "Question 2?",    // Question
-                "Answer 2", // Answer
-                "y", "-1");                  // No course tag
+                // Add FAQ question
+                "2", "-2",             // Manage FAQ → Add FAQ
+                "Topic 2",             // Topic title
+                "Question 2?",         // Question
+                "Answer 2",            // Answer
+                "y",                   // Add course tag
+                "INF2B",               // Tag with the course code
+                "-1", "-1"             // Exit FAQ and back to main menu
+        );
+
+        AdminStaffController controller = new AdminStaffController(
+                context, new TextUserInterface(), new MockAuthenticationService(), new MockEmailService()
+        );
+
+        controller.manageCourses();
+        startOutputCapture();
+        controller.manageFAQ();
+
+        assertOutputContains("New FAQ item was added with tag.");
+    }
+
+
+    /**
+     * Tests adding a new FAQ item with an invalid course tag.
+     */
+    @Test
+    public void testWithInvalidCourseTag() throws URISyntaxException, IOException, ParseException {
+        SharedContext context = new SharedContext();
+        loginAsAdminStaff(context);
+
+        setMockInput(
+                // Add a course
+                "-2", "INF2B", "SEPP", "Software Engineering and Professional Practice",
+                "n", "Mrs A", "mrsa@ed.ac.uk",
+                "Mr B", "mrb@ed.ac.uk", "1", "3", "-1", "0",
+
+                // Add a question
+                "2", "-2",             // Manage FAQ → Add FAQ
+                "Topic 2",             // Topic title
+                "Question 2?",         // Question
+                "Answer 2",            // Answer
+                "y",                   // Add course tag
+                "not-a-tag",               // Tag with the course code
+                "-1", "-1"             // Exit FAQ and back to main menu
+        );
+
+        AdminStaffController controller = new AdminStaffController(
+                context, new TextUserInterface(), new MockAuthenticationService(), new MockEmailService()
+        );
+
+        controller.manageCourses();
+        startOutputCapture();
+        controller.manageFAQ();
+
+        assertOutputContains("The tag must correspond to a course code.");
+    }
+
+
+    /**
+     * Tests adding a new FAQ item with a course tag when there are no courses in the system.
+     */
+    @Test
+    public void testNoCourses() throws URISyntaxException, IOException, ParseException {
+        SharedContext context = new SharedContext();
+        loginAsAdminStaff(context);
+
+        setMockInput("2", "-2", "Topic 2", "Question 2?", "Answer 2", "y", "-1");// No course tags in system
 
         AdminStaffController controller = new AdminStaffController(
                 context, new TextUserInterface(), new MockAuthenticationService(), new MockEmailService()
@@ -60,8 +124,7 @@ public class AddFAQQASystemTests extends TUITest {
         startOutputCapture();
         controller.manageFAQ();
 
-        assertOutputContains("Created topic 'Topic 1'");
-        assertOutputContains("Created new FAQ item");
+        assertOutputContains("No courses currently available in the system.");
     }
 
     /**
