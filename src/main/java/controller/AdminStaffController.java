16927554d37c2bacf8f61fd2af1bfb65454f61c1
Leila Fiske de Gouveia <leila.fiske@icloud.com>
2025-04-04 13:21:25 +0100

    fixed subsections and parents

--- src/main/java/controller/AdminStaffController.java
+++ src/main/java/controller/AdminStaffController.java
@@ -14,46 +14,6 @@ public class AdminStaffController extends StaffController {
     public AdminStaffController(SharedContext sharedContext, View view, AuthenticationService auth, EmailService email) {
         super(sharedContext, view, auth, email);
     }
-    public void manageFAQ2() {
-        FAQSection currentSection = null;
-
-        while (true) {
-            if (currentSection == null) {
-                view.displayFAQ(sharedContext.getFAQ());
-                view.displayInfo("[-1] Return to main menu");
-            } else {
-                view.displayFAQSection(currentSection);
-                view.displayInfo("[-1] Return to " + (currentSection.getParent() == null ? "FAQ" : currentSection.getParent().getTopic()));
-            }
-            view.displayInfo("[-2] Add FAQ item");
-            String input = view.getInput("Please choose an option: ");
-            try {
-                int optionNo = Integer.parseInt(input);
-
-                if (optionNo == -2) {
-                    addFAQItem(currentSection);
-                } else if (optionNo == -1) {
-                    if (currentSection == null) {
-                        break;
-                    } else {
-                        currentSection = currentSection.getParent();
-                    }
-                } else {
-                    try {
-                        if (currentSection == null) {
-                            currentSection = sharedContext.getFAQ().getSections().get(optionNo);
-                        } else {
-                            currentSection = currentSection.getSubsections().get(optionNo);
-                        }
-                    } catch (IndexOutOfBoundsException e) {
-                        view.displayError("Invalid option: " + optionNo);
-                    }
-                }
-            } catch (NumberFormatException e) {
-                view.displayError("Invalid option: " + input);
-            }
-        }
-    }
 
     public void manageFAQ() {
         FAQSection currentSection = null;
@@ -116,6 +76,101 @@ public class AdminStaffController extends StaffController {
         }
     }
     private void addFAQItem(FAQSection currentSection) {
+        view.displayInfo("=== Add New FAQ Question-Answer Pair===");
+
+        boolean createSection = (currentSection == null);
+        if (!createSection) {
+            createSection = view.getYesNoInput("Would you like to create a new topic for the FAQ item?");
+        }
+
+        if (createSection) {
+            String newTopic = view.getInput("Enter new topic title: ");
+            if (newTopic == null || newTopic.trim().isEmpty()) {
+                view.displayError("Topic cannot be empty");
+                return;
+            }
+
+            FAQSection newSection = new FAQSection(newTopic);
+            if (currentSection == null) {
+                if (sharedContext.getFAQ().getSections().stream().anyMatch(section -> section.getTopic().equals(newTopic))) {
+                    view.displayWarning("Topic '" + newTopic + "' already exists!");
+                    newSection = sharedContext.getFAQ().getSections().stream()
+                            .filter(section -> section.getTopic().equals(newTopic))
+                            .findFirst().orElseThrow();
+                } else {
+                    sharedContext.getFAQ().addSection(newSection);
+                    view.displayInfo("Created topic '" + newTopic + "'");
+                }
+            } else {
+                if (currentSection.getSubsections().stream().anyMatch(section -> section.getTopic().equals(newTopic))) {
+                    view.displayWarning("Topic '" + newTopic + "' already exists under '" + currentSection.getTopic() + "'!");
+                    newSection = currentSection.getSubsections().stream()
+                            .filter(section -> section.getTopic().equals(newTopic))
+                            .findFirst().orElseThrow();
+                } else {
+                    currentSection.addSubsection(newSection);
+                    view.displayInfo("Created topic '" + newTopic + "' under '" + currentSection.getTopic() + "'");
+                }
+            }
+            currentSection = newSection;
+        }
+
+        String FAQSectionTopic = currentSection.getTopic();
+
+        String question = view.getInput("Enter the question: ");
+        if (question == null || question.trim().isEmpty()) {
+            Logger.getInstance().log(System.currentTimeMillis(),
+                    ((AuthenticatedUser) sharedContext.currentUser).getEmail(),
+                    "addFAQItem", FAQSectionTopic,
+                    "FAILURE (Error: the question cannot be empty)");
+            view.displayError("Question cannot be empty");
+            return;
+        }
+
+        String answer = view.getInput("Enter the answer for new FAQ item: ");
+        if (answer == null || answer.trim().isEmpty()) {
+            Logger.getInstance().log(System.currentTimeMillis(),
+                    ((AuthenticatedUser) sharedContext.currentUser).getEmail(),
+                    "addFAQItem", FAQSectionTopic,
+                    "FAILURE (Error: the answer cannot be empty)");
+            view.displayError("Answer cannot be empty");
+            return;
+        }
+
+        boolean addTag = view.getYesNoInput("Would you like to add a Course tag?");
+        if (addTag) {
+            CourseManager courseManager = sharedContext.getCourseManager();
+
+
+            if (courseManager.viewCoursesFormatted() == "") {
+                view.displayError("No courses currently available in the system.");
+                return;
+            }
+
+            String courseTag = view.getInput("Enter course code to add as tag:");
+            if (!courseManager.hasCourse(courseTag)) {
+                Logger.getInstance().log(System.currentTimeMillis(),
+                        ((AuthenticatedUser) sharedContext.currentUser).getEmail(),
+                        "addFAQItem", FAQSectionTopic,
+                        "FAILURE (Error: the tag must correspond to a course code)");
+                view.displayError("The tag must correspond to a course code.");
+                return;
+            }
+
+            currentSection.addItem(question, answer, courseTag);
+        } else {
+            currentSection.addItem(question, answer);
+        }
+
+        Logger.getInstance().log(System.currentTimeMillis(),
+                ((AuthenticatedUser) sharedContext.currentUser).getEmail(),
+                "addFAQItem", FAQSectionTopic,
+                "SUCCESS (A new FAQ item was added)");
+
+        view.displaySuccess("The new FAQ item was added ");
+    }
+
+    private void addFAQItem2(FAQSection currentSection) {
         // display what the user is doing
         view.displayInfo("=== Add New FAQ Question-Answer Pair===");
 
