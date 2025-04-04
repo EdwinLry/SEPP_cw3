f0d608cd4142954bed6f07d8bbdd5265203cc2bc
Leila Fiske de Gouveia <leila.fiske@icloud.com>
2025-04-04 14:26:48 +0100

    fixed tests

---
+++ src/test/java/system_tests/ConsultFAQSystemTests.java
@@ -0,0 +1,165 @@
+package system_tests;
+
+import controller.AdminStaffController;
+import controller.InquirerController;
+import external.MockAuthenticationService;
+import external.MockEmailService;
+import view.TextUserInterface;
+import model.SharedContext;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+
+public class ConsultFAQSystemTests extends TUITest {
+
+    // test when there are no questions in FAQ
+    // add questions
+    // test when there is hierarchy to navigate
+    // add course tag
+    // test when there is a course tag
+    // errors: "No course found with name"
+    // "Invalid option: " + optionNo
+    // "Invalid input: " + input
+
+    @Test
+    public void testNoQuestions() throws URISyntaxException, IOException, ParseException {
+        SharedContext context = new SharedContext();
+
+        setMockInput("-1");
+
+        InquirerController controller = new InquirerController(
+                context, new TextUserInterface(), new MockAuthenticationService(), new MockEmailService()
+        );
+
+        startOutputCapture();
+        controller.consultFAQ();
+        assertOutputContains("FAQ is Empty.");
+    }
+
+    @Test
+    public void testSuccessConsult() throws URISyntaxException, IOException, ParseException {
+
+        SharedContext context = new SharedContext();
+        loginAsAdminStaff(context);
+
+        setMockInput("2", "-2", "Topic 1", "Question 1?", "Answer 1", "n", "-1", "0");
+
+        AdminStaffController adminStaff = new AdminStaffController(
+                context, new TextUserInterface(), new MockAuthenticationService(), new MockEmailService()
+        );
+
+        adminStaff.manageFAQ();
+
+        setMockInput("0", "-1", "-1");
+
+        InquirerController controller = new InquirerController(
+                context, new TextUserInterface(), new MockAuthenticationService(), new MockEmailService()
+        );
+
+        startOutputCapture();
+        controller.consultFAQ();
+
+        assertOutputContains("Topic 1");
+        assertOutputContains("Question 1?");
+        assertOutputContains("> Answer 1");
+    }
+
+
+    /**
+     * Tests Subsections and Super-sections in FAQ hierarchy and navigating it.
+     */
+    @Test
+    public void testHierarchy() throws URISyntaxException, IOException, ParseException {
+
+        SharedContext context = new SharedContext();
+        loginAsAdminStaff(context);
+
+        // First: Add Topic 1 with one QA
+        setMockInput("2", "-2", "Topic 1", "Question 1?", "Answer 1", "n",
+                // Now: Add Topic 2 as a subsection under Topic 1
+                "0", "-2", "y", "Topic 2", "Question 2?", "Answer 2", "n",
+                "-1", "-1", "-1", "0" // Exit to main menu
+        );
+
+        AdminStaffController adminStaff = new AdminStaffController(
+                context, new TextUserInterface(), new MockAuthenticationService(), new MockEmailService()
+        );
+
+        adminStaff.manageFAQ();
+
+        // Navigate to Topic 1 → Topic 2
+        setMockInput("0", "0", "-1", "-1", "-1", "-1");
+
+        InquirerController controller = new InquirerController(
+                context, new TextUserInterface(), new MockAuthenticationService(), new MockEmailService()
+        );
+
+        startOutputCapture();
+        controller.consultFAQ();
+        assertOutputContains("[-1] Return to Topic 1");
+        assertOutputContains("[-1] Return to FAQ");
+        assertOutputContains("Direct Super-Topics:");
+        assertOutputContains("Topic 1");
+        assertOutputContains("Direct Subsections:");
+        assertOutputContains("[0] Topic 2");
+    }
+
+    /**
+     * Tests navigating the hierarchy putting an invalid option
+     */
+    @Test
+    public void testInvalidOptionNumber() throws URISyntaxException, IOException, ParseException {
+        SharedContext context = new SharedContext();
+        loginAsAdminStaff(context);
+
+        setMockInput("2", "-2", "Topic", "Question", "Answer", "n", "-1");
+
+        AdminStaffController admin = new AdminStaffController(
+                context, new TextUserInterface(), new MockAuthenticationService(), new MockEmailService()
+        );
+        admin.manageFAQ();
+
+        setMockInput("5", "-1");
+
+        InquirerController controller = new InquirerController(
+                context, new TextUserInterface(), new MockAuthenticationService(), new MockEmailService()
+        );
+
+        startOutputCapture();
+        controller.consultFAQ();
+
+        assertOutputContains("Invalid option: 5");
+    }
+
+    /**
+     * Tests navigating the hierarchy putting an invalid input
+     */
+    @Test
+    public void testInvalidInputString() throws URISyntaxException, IOException, ParseException {
+        SharedContext context = new SharedContext();
+        loginAsAdminStaff(context);
+
+        setMockInput("2", "-2", "Topic", "Question", "Answer", "n", "-1");
+
+        AdminStaffController admin = new AdminStaffController(
+                context, new TextUserInterface(), new MockAuthenticationService(), new MockEmailService()
+        );
+        admin.manageFAQ();
+
+        setMockInput("not-a-number", "-1");
+
+        InquirerController controller = new InquirerController(
+                context, new TextUserInterface(), new MockAuthenticationService(), new MockEmailService()
+        );
+
+        startOutputCapture();
+        controller.consultFAQ();
+
+        assertOutputContains("Invalid input: not-a-number");
+    }
+
+
+
+}
