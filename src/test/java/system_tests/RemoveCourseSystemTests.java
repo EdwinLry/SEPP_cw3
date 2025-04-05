bd68bb3db4ca7ef43ace3cf4eba67cafd18f2cf5
LAMbo543 <lmorley543@outlook.com>
2025-04-05 15:06:18 +0100

    Added RemoveCourseSystemTests

---
+++ src/test/java/system_tests/RemoveCourseSystemTests.java
@@ -0,0 +1,108 @@
+package system_tests;
+
+import controller.AdminStaffController;
+import external.MockAuthenticationService;
+import external.MockEmailService;
+
+import model.SharedContext;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import view.TextUserInterface;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class RemoveCourseSystemTests extends TUITest{
+    private final SharedContext context = new SharedContext();
+    @BeforeEach
+    public void createTestCourse() throws URISyntaxException, IOException, ParseException {
+        loginAsAdminStaff(context);
+        context.getCourseManager().addCourse("INF1A", "Informatics 1", "Intro to Informatics",
+                true, "Dr X", "drx@ed.ac.uk", "Ms Y", "msy@ed.ac.uk",
+                2, 3);
+    }
+
+    @Test
+    public void testRemoveCourseSuccessfully() throws URISyntaxException, IOException, ParseException {
+        setMockInput("-4", "INF1A", "-1");
+
+        AdminStaffController controller = new AdminStaffController(
+                context,
+                new TextUserInterface(),
+                new MockAuthenticationService(),
+                new MockEmailService()
+        );
+
+        startOutputCapture();
+        controller.manageCourses();
+
+        assertOutputContains("Course removed successfully");
+    }
+
+    @Test
+    public void testRemoveCourseUnSuccessfully() throws URISyntaxException, IOException, ParseException {
+        setMockInput("-4", "INF1B", "-1");
+
+        AdminStaffController controller = new AdminStaffController(
+                context,
+                new TextUserInterface(),
+                new MockAuthenticationService(),
+                new MockEmailService()
+        );
+
+        startOutputCapture();
+        controller.manageCourses();
+        assertOutputContains("Course not found");
+    }
+
+    @Test
+    public void testUncapitalizedCourseCode() throws URISyntaxException, IOException, ParseException {
+        setMockInput("-4", "inf1a", "-1");
+
+        AdminStaffController controller = new AdminStaffController(
+                context,
+                new TextUserInterface(),
+                new MockAuthenticationService(),
+                new MockEmailService()
+        );
+
+        startOutputCapture();
+        controller.manageCourses();
+        assertOutputContains("Course not found");
+    }
+
+    @Test
+    public void testNoCourseToBeRemoved() throws URISyntaxException, IOException, ParseException {
+        context.courseManager.removeCourse("INF1A");
+
+        setMockInput("-4", "-1");
+
+        AdminStaffController controller = new AdminStaffController(
+                context,
+                new TextUserInterface(),
+                new MockAuthenticationService(),
+                new MockEmailService()
+        );
+
+        startOutputCapture();
+        controller.manageCourses();
+        assertOutputContains("No courses available");
+    }
+
+    @Test
+    public void testNullInput() throws URISyntaxException, IOException, ParseException {
+        setMockInput("-4", null,"-1");
+
+        AdminStaffController controller = new AdminStaffController(
+                context,
+                new TextUserInterface(),
+                new MockAuthenticationService(),
+                new MockEmailService()
+        );
+
+        startOutputCapture();
+        controller.manageCourses();
+        assertOutputContains("Course not found");
+    }
+}
